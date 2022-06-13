e25f1ed2592a493169a2b1d3d0e873756683b4fa
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-13 13:20:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+            console.log(document.querySelector('form'))
+            document.querySelector('form').addEventListener('submit', (e) => {
+                    e.preventDefault()
+                    handleToDo(e.target.new_todo.value)
+                }
+            });
+
+        function handleToDo(todo) {
+            let p = document.createElement('p')
+            p.textContent = todo
+            document.querySelector('#todo_container').appendChild('ps')
+        }
